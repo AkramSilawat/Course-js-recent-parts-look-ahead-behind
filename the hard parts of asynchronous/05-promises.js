420ec819b60203344889eaafe11085e7178e4572
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-01-27 22:00:43 +0530

    promises

--- the hard parts of asynchronous/05-promises.js
+++ the hard parts of asynchronous/05-promises.js
@@ -36,3 +36,112 @@
 // futureData.then(display);
 
 // console.log("Me frst!")
+
+
+
+
+
+// ------- Web APIs & Promises Example: fetch -------- //
+
+// But we need to know how our promises-deferred functionality gets back into javaScript to be run //
+
+
+// function display(data) { console.log(data);}
+
+// function printHello() { console.log("Hello"); }
+
+// function blockFor300ms() { /*block js thread for 300ms*/}
+
+// setTimeout(printHello,0);
+
+// const futureData = fetch('https:// twitter.com/will/tweets/1');
+// futureData.then(display);
+
+// blockFor300ms();
+// console.log("Me frst!")
+
+
+
+// ------- Web APIs & Promises Example: then -------- //
+
+// But we need to know how our promises-deferred functionality gets back into javaScript to be run //
+
+
+// function display(data) { console.log(data);}
+
+// function printHello() { console.log("Hello"); }
+
+// function blockFor300ms() { /*block js thread for 300ms*/}
+
+// setTimeout(printHello,0);
+
+// const futureData = fetch('https:// twitter.com/will/tweets/1');
+// futureData.then(display);
+
+// blockFor300ms();
+// console.log("Me frst!")
+
+
+
+
+
+// ------- Web APIs & Promises Example: Microtask Queue -------- //
+
+// But we need to know how our promises-deferred functionality gets back into javaScript to be run //
+
+
+// function display(data) { console.log(data);}
+
+// function printHello() { console.log("Hello"); }
+
+// function blockFor300ms() { /*block js thread for 300ms*/}
+
+// setTimeout(printHello,0);
+
+// const futureData = fetch('https:// twitter.com/will/tweets/1');
+// futureData.then(display);
+
+// blockFor300ms();
+// console.log("Me frst!")
+
+
+
+
+// ------- Promises And Asynchronous Q&A -------- //
+
+// But we need to know how our promises-deferred functionality gets back into javaScript to be run //
+
+
+// function display(data) { console.log(data);}
+
+// function printHello() { console.log("Hello"); }
+
+// function blockFor300ms() { /*block js thread for 300ms*/}
+
+// setTimeout(printHello,0);
+
+// const futureData = fetch('https:// twitter.com/will/tweets/1');
+// futureData.then(display);
+
+// blockFor300ms();
+// console.log("Me frst!")
+
+
+
+
+
+
+// ----- Promises Review ------ //
+
+// == Problems
+
+// - 99% of developers have no idea how then're working under the hood
+// - Debugging becomes super-hard as a result 
+// - developers fail techical interviews
+
+// == Benefits
+
+// - Cleaner readable style with psecudo-synchronous style code
+// - Nice error handling process
+
+
